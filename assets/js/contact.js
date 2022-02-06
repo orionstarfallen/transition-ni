22fe8c5178d0d6ea4829f5f094a61f15dcd2c976
orionstarfallen <orionstarfallen@gmail.com>
2022-02-06 23:23:23 +0000

    contact fixes

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,16 +1,29 @@
 function invalidInput(){
     var form = document.getElementById('form');
-    var x = document.getElementById('first-name');  
+    var x = document.getElementById('name');  
     var y = document.getElementById('email');  
     var z = document.getElementById('message');  
 
-    x.classList.add("submitted");
-    y.classList.add("submitted");
-    z.classList.add("submitted");
+    x.classList.add('submitted');
+    y.classList.add('submitted');
+    z.classList.add('submitted');
 
-
-    if(form.valid()){
+    if(form.valid()) {
         form.submit();
         form.reset();
     }
 }
+
+function toggleLight(){
+    var form = document.getElementById('form');
+    var mail = document.getElementById('mail-box');
+
+    mail.style.background = 'rgba(0, 0, 0, 1)';
+}
+
+  function toggleDark(){
+    var form = document.getElementById('form');
+    var mail = document.getElementById('mail-box');
+
+    mail.style.background = 'rgba(255, 255, 255, 0.1)';
+  }
